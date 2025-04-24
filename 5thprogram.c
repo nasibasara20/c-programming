04a486376d008c73636a17cd0be3b6dc991a3786
NishatTasnim <nasibasara20@gmail.com>
2025-04-24 14:00:21 +0600

    Add files via upload

---
+++ 5thprogram.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+double a,b;
+
+scanf("%lf %lf",&a,&b);
+double mul1=a*3.5;
+double mul2=b*7.5;
+double sum=mul1+mul2;
+double sum2=3.5+7.5;
+double div=sum/sum2;
+printf("MEDIA = %.5lf\n",div);
+}
